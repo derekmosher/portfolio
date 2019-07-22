76da92a401301db399db4075b26d6bff808f87c4
derek mosher <derekmosher@dereks-MacBook-Pro.local>
2019-07-22 14:30:23 -0700

    more updates

--- build/precache-manifest.3a4f5fcaf4c96dee3cca9417cdc28118.js
+++ build/precache-manifest.3a4f5fcaf4c96dee3cca9417cdc28118.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "62ffc295c7527301f014286461d676af",
+    "revision": "54c6eb40d689fda58091285d533d2aae",
     "url": "/index.html"
   },
   {
-    "revision": "a7ffbbd308e53ce9100e",
-    "url": "/static/css/main.b1ac7167.chunk.css"
+    "revision": "937aed874d46f9272061",
+    "url": "/static/css/main.0de25125.chunk.css"
   },
   {
     "revision": "2cfa9e7d10812add490c",
     "url": "/static/js/2.aa255b42.chunk.js"
   },
   {
-    "revision": "a7ffbbd308e53ce9100e",
-    "url": "/static/js/main.ac9234b8.chunk.js"
+    "revision": "937aed874d46f9272061",
+    "url": "/static/js/main.18707eb5.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
